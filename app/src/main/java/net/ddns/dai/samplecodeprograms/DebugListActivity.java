a3146e8958c3f8b5668adae38f07bec186eff45a
Dai <dai7774@gmail.com>
2018-03-25 11:15:43 +0900

    create ButtonSampleActivity

--- app/src/main/java/net/ddns/dai/samplecodeprograms/DebugListActivity.java
+++ app/src/main/java/net/ddns/dai/samplecodeprograms/DebugListActivity.java
@@ -43,7 +43,7 @@ public class DebugListActivity extends AppCompatActivity {
             }
         }
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
