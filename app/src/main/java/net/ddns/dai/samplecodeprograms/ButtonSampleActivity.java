e7ae77143599052d40274a690aa740b64be688de
Dai <dai7774@gmail.com>
2018-03-28 20:57:10 +0900

    add ChromeCustomTab

--- app/src/main/java/net/ddns/dai/samplecodeprograms/ButtonSampleActivity.java
+++ app/src/main/java/net/ddns/dai/samplecodeprograms/ButtonSampleActivity.java
@@ -1,8 +1,15 @@
 package net.ddns.dai.samplecodeprograms;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -76,10 +83,36 @@ public class ButtonSampleActivity extends AppCompatActivity implements View.OnCl
                 return true;
 
             case R.id.menuItem:
-                //TODO WebViewで解説ページへ
+                String linkText = "https://qiita.com/daivr7774/items/3bed5793a124aa4a44aa";
+                chromeBrowseTab(Uri.parse(linkText));
+
                 return true;
         }
         return super.onOptionsItemSelected(menuItem);
+    }
+
+    private void chromeBrowseTab(Uri uri){
+
+        Intent intent = new Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, uri.toString());
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_share);
+
+        CustomTabsIntent tabsIntent = new CustomTabsIntent.Builder()
+                .setShowTitle(true)
+                .setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                .setStartAnimations(this, android.R.anim.slide_out_right, android.R.anim.slide_in_left)
+                .setExitAnimations(this, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                .setCloseButtonIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_arrow_back))
+                .setActionButton(icon, "arrow_back", pendingIntent)
+                .addMenuItem("共有", pendingIntent)
+                .build();
+
+        tabsIntent.launchUrl(this, uri);
+
     }
 
 }
