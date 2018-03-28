bf0fc3b79fa178e8454a5f1ce5a65c5f5896170a
Dai <dai7774@gmail.com>
2018-03-28 19:08:46 +0900

    add ChromeCustomTab & complete Task

--- app/src/main/java/net/ddns/dai/samplecodeprograms/ButtonSampleActivity.kt
+++ app/src/main/java/net/ddns/dai/samplecodeprograms/ButtonSampleActivity.kt
@@ -1,8 +1,15 @@
 package net.ddns.dai.samplecodeprograms
 
 import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
 import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -65,11 +72,39 @@ class ButtonSampleActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.menuItem -> {
-                //TODO WebViewで解説ページへ
+                val linkText = "https://qiita.com/daivr7774/items/3bed5793a124aa4a44aa"
+                chromeBrowseTab(Uri.parse(linkText))
+
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("PrivateResource")
+    private fun chromeBrowseTab(uri: Uri){
+
+        val  intent = Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .putExtra(Intent.EXTRA_TEXT, uri.toString())
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val icon = BitmapFactory.decodeResource(Resources.getSystem(), R.drawable.ic_share)
+
+        val tabsIntent = CustomTabsIntent.Builder()
+                .setShowTitle(true)
+                .setToolbarColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+                .setStartAnimations(this, R.anim.abc_slide_in_bottom, R.anim.abc_slide_out_top)
+                .setExitAnimations(this, R.anim.abc_slide_in_top, R.anim.abc_slide_out_bottom)
+                .setCloseButtonIcon(BitmapFactory.decodeResource(Resources.getSystem(), R.drawable.ic_arrow_back))
+                .setActionButton(icon, "arrow_back", pendingIntent)
+                .addMenuItem("共有", pendingIntent)
+                .build()
+
+        //Chrome起動
+        tabsIntent.launchUrl(this, uri)
     }
 
 }
