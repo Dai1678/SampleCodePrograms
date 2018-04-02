82e5720f621112c94d8917ff9bd35d949ba039cf
Dai <dai7774@gmail.com>
2018-04-02 13:42:47 +0900

    create TextSampleActivity.java

---
+++ app/src/main/java/net/ddns/dai/samplecodeprograms/TextSampleActivity.java
@@ -0,0 +1,13 @@
+package net.ddns.dai.samplecodeprograms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_sample);
+    }
+}
